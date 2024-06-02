d7e8861767ecfc38b9933676f1c3c8cb6cbe41a3
Ankurdyno <98178905+Ankurdyno@users.noreply.github.com>
2024-06-02 13:02:41 +0530

    feat : wishlist add item and show item in wishlist page

--- src/pages/WatchlistPage.jsx
+++ src/pages/WatchlistPage.jsx
@@ -2,23 +2,42 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/common/Navbar";
 import TabsComponent from "../components/Dashboard/TabsComponent";
 import { get100Coins } from "../functions/get100Coins";
+import Loader from "./../components/common/Loader";
 
 function WatchlistPage() {
-  const [coins,setCoins]=useState();
-  useEffect(()=>{
-  getData();
-  },[])
+  const [coins, setCoins] = useState([]);
+  const [wishListedData, SetWishListedData] = useState(() => {
+    return localStorage.getItem("wishlist");
+  });
+  const [isLoading, setIsLoading] = useState(true);
 
-  const getData= async()=>{
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
     const responce = await get100Coins();
-    setCoins(responce.data)
-    console.log(responce.data)
-  }
-  
+    setCoins(getWishlistedCoins(responce.data, wishListedData));
+    setIsLoading(false);
+    console.log(wishListedData);
+    console.log(coins);
+  };
+
+  const getWishlistedCoins = (data, wishlist) => {
+    return data.filter((coin) => wishlist.includes(coin.id));
+  };
+
+
   return (
     <div>
       <Navbar />
-      {/* <TabsComponent coins={coins}/> */}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <TabsComponent coins={coins} />
+        </>
+      )}
     </div>
   );
 }
